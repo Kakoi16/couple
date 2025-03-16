74fc7791b78f35c84a1dde42d1e50251f459a314
Kakoi16 <hotaruuuchan2001@gmail.com>
2025-03-16 09:43:43 +0700

    Migrate One

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const Database = require('better-sqlite3');
-const db = new Database('./database.db');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+});
 
 const router = express.Router();
 
